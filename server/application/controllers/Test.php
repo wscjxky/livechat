03ce4f8a2585feac2f7f638c97e78b81f74ee5f9
wscjxkym <619677632@qq.com>
2018-02-23 16:03:55 +0800

    init

---
+++ server/application/controllers/Test.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+
+class Test extends CI_Controller {
+   public function index(){
+       $this->json(
+           [
+               'code'=>0,
+               'data'=>[
+                   'msg'=>'Hello Wolrd'
+               ]
+           ]
+       );
+   }
+}
